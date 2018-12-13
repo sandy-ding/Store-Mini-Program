e5253a4b9d3340c4f2dadab90672efc550db75a7
Sandy <dhl15821939286@gmail.com>
2018-12-14 00:03:21 +0800

    U2

---
+++ server/controllers/product.js
@@ -0,0 +1,8 @@
+const DB = require('../utils/db.js')
+
+module.exports = {
+  list: async ctx => {
+    ctx.state.data = await DB.query("SELECT * FROM product;")
+    console.log(ctx.state.data)
+  }
+}
